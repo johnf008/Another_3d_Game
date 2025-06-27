a86e6517dea1cbabdf82b051a716310766beeff5
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-27 15:30:12 -0500

    Moved camera outside of player

--- player/player.gd
+++ player/player.gd
@@ -4,8 +4,10 @@ func _physics_process(delta: float) -> void:
 	const FLY_FALL_FACTOR = 5
 	 
 	velocity.y -= 20.0 * delta
+	rotation.z -= 1
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = 10.0
+		rotation.z += 1
 		
 	
 	move_and_slide()
