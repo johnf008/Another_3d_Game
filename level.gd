82174087968cb6ecf2d4e466e74cdcd8b9724d5f
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-27 23:07:19 -0500

    Starting work on the level spawner (spawns the modules)

---
+++ level.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var modules: Array[PackedScene] = []
+var amount = 10
+var random = RandomNumberGenerator.new()
+var offset = 50
+
+func _ready():
+	for pos in offset:
+		spawnModule(pos * offset)
+		
+func spawnModule(pos):
+	random.randomize()
+	var num = random.randi_range(0, modules.size() - 1)
+	var instance = modules[num].instantiate()
+	instance.position.x = pos
+	add_child(instance)
