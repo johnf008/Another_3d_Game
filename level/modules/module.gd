99a06a3cb97a6beff61b2bf9bd0b5712cf7b9fae
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-29 00:03:30 -0500

    So my area3D is detecting collision to push the player, but not print the statement :(

--- level/modules/module.gd
+++ level/modules/module.gd
@@ -8,3 +8,9 @@ func _process(delta):
 	if(position.x < -37):
 		level.spawnModule(position.x + (level.amount * level.offset))
 		queue_free()
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.is_in_group("Player"):
+		print("Player has collided & i didn't crash :3 ")
+	print("Well something is inside of me")
