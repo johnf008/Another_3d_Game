99442217f2456e8bc76a6f270ce3027ccdf1aa2b
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-29 18:44:23 -0500

    Now when the player hits one of the pipes, the modules will stop and the player will fall. In addition, everything gets rest upon the reset button being pressed

--- restart_menu.gd
+++ restart_menu.gd
@@ -1,12 +1,22 @@
 extends Control
+@onready var level: Node3D = %Level
+
 
 func stop():
 	get_tree().paused = true
+
+func stop_incoming_pipes():
+	%Level.process_mode = Node.PROCESS_MODE_DISABLED
+
 func _on_button_pressed() -> void:
 	get_tree().reload_current_scene()
 
-
 func _on_character_body_3d_display_restart_menu() -> void:
 	self.show()	
+	stop_incoming_pipes()
+	
+	
+	
+
 	
 	
