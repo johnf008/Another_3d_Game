f114cb92f62703abcfc0cab7e97da82d80fde5da
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-29 21:33:59 -0500

    Collision on the coin point works :D

--- level/modules/module.gd
+++ level/modules/module.gd
@@ -24,3 +24,8 @@ func _process(delta):
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
 		player_collide.emit()
+
+
+func _on_area_3d_coin_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		print("You touched me :/")
